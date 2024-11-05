ba49d3f16b9301c088270e862c515453a55d0746
catalinbroinas <catalinbroinas@yahoo.com>
2024-11-05 13:54:03 +0200

    Project setup: add js modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
+// Import styles
 import '../css/styles.css';
+
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
 import '@fortawesome/fontawesome-free/css/brands.css';
 import '@fortawesome/fontawesome-free/css/solid.css';
+
+// Import modules
+import { Ship } from './modules/ship';
+import { Gameboard } from './modules/gameboard';
+import { Player } from './modules/player';
---
+++ src/js/modules/ship.js
@@ -0,0 +1,5 @@
+function Ship() {
+
+}
+
+export { Ship };
