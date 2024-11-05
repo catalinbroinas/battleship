f4313505d84c4e4ee616b52e94b81c318d18580d
catalinbroinas <catalinbroinas@yahoo.com>
2024-11-05 14:37:27 +0200

    feat: create `Ship` object with hit and isSunk methods

--- src/js/modules/ship.js
+++ src/js/modules/ship.js
@@ -1,5 +1,19 @@
-function Ship() {
+function Ship(length) {
+    const ship = {
+        length,
+        hits: 0,
+        sunk: false,
+        hit: function () {
+            this.hits += 1;
+            this.isSunk();
+        },
+        isSunk: function () {
+            this.sunk = this.hits === this.length;
+            return this.sunk
+        }
+    };
 
+    return ship;
 }
 
 export { Ship };
