467d23490877f5a7cf09e58e15463631204528d9
catalinbroinas <catalinbroinas@yahoo.com>
2024-11-06 12:20:31 +0200

    feat: add `initGameboard` method to `Gameboard`
    - Initialize an array 10x10 with null values

--- src/js/modules/gameboard.js
+++ src/js/modules/gameboard.js
@@ -1,5 +1,14 @@
 function Gameboard() {
+    const initGameboard = () => {
+        const rows = 10;
+        const columns = 10;
+        const board = Array.from(Array(rows), () => Array(columns).fill(null));
+        return board;
+    };
 
+    return {
+        initGameboard
+    };
 }
 
 export { Gameboard };
