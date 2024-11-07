c9b0f72fc9e0e9b245acd240decd406ec7b63c66
catalinbroinas <catalinbroinas@yahoo.com>
2024-11-07 13:45:36 +0200

    refactor: improve `placeShip` and `receiveAttack`
    - `placeShip` method handles errors and returns status
    - `receiveAttack` method return the outcome of the attack

--- src/js/modules/gameboard.js
+++ src/js/modules/gameboard.js
@@ -16,56 +16,53 @@ function Gameboard() {
 
         // Validate the ship length
         if (!length) {
-            throw new Error('Select a valid ship');
+            return 'Select a valid ship';
         }
 
         // Check if the place is within the board bounds
         if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
-            throw new Error('Place is out of bounds');
+            return 'Place is out of bounds';
         }
 
         // Check if the ship fits in the space on the board
         if (col + length >= board[0].length) {
-            throw new Error('Ship does not fit in the selected space');
+            return 'Ship does not fit in the selected space';
         }
 
         // Check if the space is already occupied by another ship
         const emptyPlace = Array.from({ length }, (_, i) => board[row][col + i]
         ).some((cell) => cell !== null);
         if (emptyPlace) {
-            throw new Error('This place is already occupied');
+            return 'This place is already occupied';
         }
 
         // Place the ship on the board
         Array.from({ length }, (_, i) => board[row][col + i] = 1);
-
         return true;
     };
 
-    // Places an attack on the gameboard at the specified location
+    // Processes an attack on the gameboard at the specified location
     const receiveAttack = (place) => {
         const { row, col } = place;
 
-        // Validate the attack is within the board bounds
+        // Check if the attack is within the board boundaries
         if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
-            throw new Error('Place is out of bounds');
+            return 'Place is out of bounds';
         }
 
-        // If the place was already attacked, return a message
-        if (board[row][col] === 0) {
-            return 'This place already attacked';
-        }
+        // If the place was already attacked, do nothing
+        if (board[row][col] === 0) return;
 
-        // If no ship is present, mark as missed
+        // Mark as missed if no ship is present
         if (board[row][col] === null) {
             board[row][col] = 0;
-            return 'Missed attack';
+            return 0;
         }
 
-        // If a ship is present, mark as hit
+        // Mark as hit if a ship is present
         if (board[row][col] === 1) {
             board[row][col] = 0;
-            return 'Hit';
+            return 1;
         }
     };
 
