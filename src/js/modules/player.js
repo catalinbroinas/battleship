45934a62c914d087091a38277476a3645402be5b
catalinbroinas <catalinbroinas@yahoo.com>
2024-11-07 14:36:40 +0200

    feat: add `board` and `ships` properties to `Player`

--- src/js/modules/player.js
+++ src/js/modules/player.js
@@ -1,5 +1,20 @@
-function Player() {
+import { Ship } from "./ship";
+import { Gameboard } from "./gameboard";
 
+function Player() {
+    const gameboard = Gameboard();
+
+    // Gameboard of the player
+    const board = gameboard.getBoard();
+
+    // List of ships
+    const ships = [
+        Ship(5),  // Carrier
+        Ship(4),  // Battleship
+        Ship(3),  // Cruiser
+        Ship(3),  // Submarine
+        Ship(2)   // Destroyer
+    ];
 }
 
 export { Player };
