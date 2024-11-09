4147cc9e623a09d4c33b7f2d439e5e275e0c1055
catalinbroinas <catalinbroinas@yahoo.com>
2024-11-09 13:43:12 +0200

    feat: add `player.test.js` with initial configuration

---
+++ src/js/tests/player.test.js
@@ -0,0 +1,11 @@
+import { Ship } from "../modules/ship";
+import { Gameboard } from "../modules/gameboard";
+
+describe('Player factory function', () => {
+    let ship1;
+    let player1, player2;
+
+    beforeEach(() => {
+        ship1 = Ship(3);
+    });
+});
