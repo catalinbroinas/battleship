256a01b336e22f4b70016e48800d6eb4f4c557de
catalinbroinas <catalinbroinas@yahoo.com>
2024-11-14 12:29:21 +0200

    feat: add `Game` factory function with core methods
    - Methods: createPlayer, initGame, startGame, playerTurn, and checkWinner
    - Set up foundational structure for game management

---
+++ src/js/modules/game.js
@@ -0,0 +1,35 @@
+import { Player } from "./player";
+
+function Game() {
+    let player1;
+    let player2;
+    let currentPlayer;
+
+    const createPlayer = (name, type) => Player(name, type);
+
+    const initGame = (playerName, computerName) => {
+
+    };
+
+    const startGame = () => {
+
+    };
+
+    const playerTurn = (place) => {
+
+    };
+
+    const checkWinner = () => {
+
+    };
+
+    return {
+        createPlayer,
+        initGame,
+        startGame,
+        playerTurn,
+        checkWinner
+    };
+}
+
+export { Game };
